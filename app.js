89b980a6f3615f29837c352228805b2c0c075d5b
FranckS <ndzomgafs@gmail.com>
2022-04-23 23:58:06 +0200

    change message 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Franck S!'));
+app.get('/', (req, res) => res.send('Hello Franck Stéphane Ndz!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
